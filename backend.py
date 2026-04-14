00f054b7814c030778f3cfa1325648396b47e180
ziaulislam-mughal <ziaulislammughal0@gmail.com>
2026-04-14 22:30:55 +0500

    Backend of Chatbot

---
+++ backend.py
@@ -0,0 +1,79 @@
+from langgraph.graph import StateGraph , START , END 
+from typing import TypedDict , Annotated
+import os 
+from dotenv import load_dotenv
+from langchain_huggingface import HuggingFaceEndpoint , ChatHuggingFace
+from langchain_core.messages import BaseMessage
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.graph.message import add_messages
+
+
+# ====================================================================
+                            # load modules
+# ====================================================================
+#load environment 
+load_dotenv()
+#check token 
+token = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+if not token:
+    print("Error: HUGGINGFACEHUB_API_TOKEN is not set in the .env file.")
+else:
+    print("Done")
+
+#  Setup the Endpoint
+repo_id = "meta-llama/Llama-3.1-8B-Instruct"
+
+llm_H = HuggingFaceEndpoint(
+    repo_id=repo_id,
+    task="text-generation",
+    max_new_tokens=512,
+    temperature=0.7,
+    huggingfacehub_api_token=token,
+)
+
+#  Wrap it in ChatHuggingFace (This handles the "conversational" format for you)
+llm = ChatHuggingFace(llm=llm_H)
+# ====================================================================
+                            # Define Schema
+# ====================================================================
+class ChatState(TypedDict):
+    messages : Annotated[list[BaseMessage] , add_messages]
+# ====================================================================
+                            # Function
+# ====================================================================
+#function 
+def chat(state:ChatState):
+    #user query 
+    messages =  state['messages']
+    #llm 
+    response = llm.invoke(messages)
+    #response
+    return {"messages": [response]}
+
+# ====================================================================
+                            # Checkpointer
+# ====================================================================
+#checkpointer 
+checkpointer = InMemorySaver()
+# ====================================================================
+                            # define Graph
+# ====================================================================
+#Graph define 
+graph  = StateGraph(ChatState)
+# ====================================================================
+                            # define Nodes
+# ====================================================================
+# nodes 
+graph.add_node("chat",chat)
+# ====================================================================
+                            # define Edges
+# ====================================================================
+#edge
+graph.add_edge(START,"chat")
+graph.add_edge("chat",END)
+# ====================================================================
+                            # Compile Graph
+# ====================================================================
+chatbot = graph.compile(checkpointer=checkpointer)
+
+#
